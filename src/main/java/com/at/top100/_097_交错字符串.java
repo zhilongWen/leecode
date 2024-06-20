22e1d3564430621479561992270012c6dddf3c3a
wenzhilong <wenzhilong@wenzhilong.com>
2024-06-20 23:34:40 +0800

    top100 _097_交错字符串

---
+++ src/main/java/com/at/top100/_097_交错字符串.java
@@ -0,0 +1,35 @@
+package com.at.top100;
+
+// https://leetcode.cn/problems/interleaving-string/description/
+
+public class _097_交错字符串 {
+
+    public boolean isInterleave(String s1, String s2, String s3) {
+
+        int len1 = s1.length(), len2 = s2.length(), len3 = s3.length();
+
+        if (len1 + len2 != len3) {
+            return false;
+        }
+
+        boolean[][] dp = new boolean[len1 + 1][len2 + 1];
+        dp[0][0] = true;
+
+        for (int i = 0; i <= len1; i++) {
+            for (int j = 0; j <= len2; j++) {
+
+                int p = i + j - 1;
+
+                if (i > 0) {
+                    dp[i][j] = dp[i][j] || (dp[i - 1][j] && s1.charAt(i - 1) == s3.charAt(p));
+                }
+
+                if (j > 0) {
+                    dp[i][j] = dp[i][j] || (dp[i][j - 1] && s2.charAt(j - 1) == s3.charAt(p));
+                }
+            }
+        }
+
+        return dp[len1][len2];
+    }
+}
