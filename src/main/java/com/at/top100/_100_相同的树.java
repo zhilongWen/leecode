f452d62a86eb0d9bb905dc989dd86643eedffece
wenzhilong <wenzhilong@wenzhilong.com>
2024-06-24 21:25:34 +0800

    top100 _100_相同的树

---
+++ src/main/java/com/at/top100/_100_相同的树.java
@@ -0,0 +1,25 @@
+package com.at.top100;
+
+// https://leetcode.cn/problems/same-tree/description/
+
+import com.at.common.TreeNode;
+
+public class _100_相同的树 {
+
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+
+        if (p == null && q == null) {
+            return true;
+        }
+
+        if (p == null || q == null) {
+            return false;
+        }
+
+        if (p.val != q.val) {
+            return false;
+        }
+
+        return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+    }
+}
